0fc06c5d22b763b017750fef0efc4085b48dd27c
Johnny <johnkcli@hotmail.com>
2023-06-11 23:20:06 +0100

    plays sound from url through earpiece on ios and android

--- components/Listen.js
+++ components/Listen.js
@@ -1,6 +1,7 @@
 import React, {useState, useContext} from 'react';
-import {View, Text, DeviceEventEmitter} from 'react-native';
+import {View, Text, DeviceEventEmitter, StyleSheet} from 'react-native';
 
+import Sound from 'react-native-sound';
 import Button from './Button';
 import {ConnectedContext} from '../context/ConnectedContext';
 import InCallManager from 'react-native-incall-manager';
@@ -8,6 +9,23 @@ import InCallManager from 'react-native-incall-manager';
 export default ({navigation}) => {
   const connectedState = useContext(ConnectedContext);
   const [isNear, setIsNear] = useState(false);
+  const [track, setTrack] = useState(null);
+  React.useEffect(() => {
+    Sound.setCategory('Voice');
+    const track = new Sound(
+      'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+      null,
+      e => {
+        if (e) {
+          console.log('error loading track:', e);
+        } else {
+          track.setVolume(1);
+          track.play();
+          setTrack(track);
+        }
+      },
+    );
+  }, []);
 
   DeviceEventEmitter.addListener('Proximity', function (data) {
     setIsNear(data['isNear']);
@@ -24,12 +42,14 @@ export default ({navigation}) => {
   }
   if (!isNear) {
     return (
-      <View style>
+      <View style={{marginTop: '25%'}}>
         <Button
           title="Back"
           onPress={() => {
             InCallManager.stop();
             navigation.goBack();
+            track.pause();
+            track.release();
           }}
         />
       </View>
@@ -38,3 +58,15 @@ export default ({navigation}) => {
     return <View></View>;
   }
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  screenText: {
+    fontSize: 20,
+    color: 'white',
+  },
+});
