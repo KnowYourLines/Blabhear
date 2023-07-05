5e98d57b34f18928e762c0ee60a882404e2e1f89
Johnny <johnkcli@hotmail.com>
2023-07-05 18:53:23 +0100

    new slider and pause/play working for listening

--- components/Listen.js
+++ components/Listen.js
@@ -4,10 +4,9 @@ import {
   DeviceEventEmitter,
   Platform,
   StyleSheet,
-  TouchableOpacity,
   Text,
-  Dimensions,
 } from 'react-native';
+import Slider from '@react-native-community/slider';
 
 import Sound from 'react-native-sound';
 import Button from './Button';
@@ -16,8 +15,10 @@ import InCallManager from 'react-native-incall-manager';
 export default ({navigation}) => {
   const [isNear, setIsNear] = useState(false);
   const [track, setTrack] = useState(null);
-  const [playTime, setPlayTime] = useState('00:00:00');
-  const [duration, setDuration] = useState('00:00:00');
+  const [playSeconds, setPlaySeconds] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [sliderEditing, setSliderEditing] = useState(false);
+  const [timeout, setTimeout] = useState(null);
   const [isPlaying, setIsPlaying] = useState(true);
   const onStartPlay = async e => {
     setIsPlaying(true);
@@ -44,11 +45,47 @@ export default ({navigation}) => {
           track.setVolume(1);
           track.play();
           setTrack(track);
+          setDuration(track.getDuration());
         }
       },
     );
+    setTimeout(
+      setInterval(() => {
+        if (track && track.isLoaded() && isPlaying && !sliderEditing) {
+          track.getCurrentTime((seconds, trackIsPlaying) => {
+            setPlaySeconds(seconds);
+          });
+        }
+      }, 100),
+    );
   }, []);
+  const onSliderEditStart = () => {
+    setSliderEditing(true);
+  };
+  const onSliderEditEnd = () => {
+    setSliderEditing(false);
+  };
+  const onSliderEditing = value => {
+    if (track) {
+      track.setCurrentTime(value);
+      setPlaySeconds(value);
+    }
+  };
+  const getAudioTimeString = seconds => {
+    const h = parseInt(seconds / (60 * 60));
+    const m = parseInt((seconds % (60 * 60)) / 60);
+    const s = parseInt(seconds % 60);
 
+    return (
+      (h < 10 ? '0' + h : h) +
+      ':' +
+      (m < 10 ? '0' + m : m) +
+      ':' +
+      (s < 10 ? '0' + s : s)
+    );
+  };
+  const currentTimeString = getAudioTimeString(playSeconds);
+  const durationString = getAudioTimeString(duration);
   DeviceEventEmitter.addListener('Proximity', function (data) {
     setIsNear(data['isNear']);
   });
@@ -62,8 +99,39 @@ export default ({navigation}) => {
             navigation.goBack();
             track.pause();
             track.release();
+            if (timeout) {
+              clearInterval(timeout);
+            }
           }}
         />
+        <View
+          style={{
+            marginVertical: 15,
+            marginHorizontal: 15,
+            flexDirection: 'row',
+          }}>
+          <Text style={{color: 'white', alignSelf: 'center'}}>
+            {currentTimeString}
+          </Text>
+          <Slider
+            onTouchStart={onSliderEditStart}
+            onTouchEnd={onSliderEditEnd}
+            onValueChange={onSliderEditing}
+            value={playSeconds}
+            maximumValue={duration}
+            maximumTrackTintColor="gray"
+            minimumTrackTintColor="white"
+            thumbTintColor="white"
+            style={{
+              flex: 1,
+              alignSelf: 'center',
+              marginHorizontal: Platform.select({ios: 5}),
+            }}
+          />
+          <Text style={{color: 'white', alignSelf: 'center'}}>
+            {durationString}
+          </Text>
+        </View>
         <View style={styles.viewPlayer}>
           <View style={styles.playBtnWrapper}>
             <Button
