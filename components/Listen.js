330a5a16de7de5a4732baa401470b889fa13970c
Johnny <johnkcli@hotmail.com>
2023-07-05 18:23:16 +0100

    can pause and resume track

--- components/Listen.js
+++ components/Listen.js
@@ -1,5 +1,13 @@
 import React, {useState} from 'react';
-import {View, DeviceEventEmitter, Platform} from 'react-native';
+import {
+  View,
+  DeviceEventEmitter,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Dimensions,
+} from 'react-native';
 
 import Sound from 'react-native-sound';
 import Button from './Button';
@@ -8,6 +16,17 @@ import InCallManager from 'react-native-incall-manager';
 export default ({navigation}) => {
   const [isNear, setIsNear] = useState(false);
   const [track, setTrack] = useState(null);
+  const [playTime, setPlayTime] = useState('00:00:00');
+  const [duration, setDuration] = useState('00:00:00');
+  const [isPlaying, setIsPlaying] = useState(true);
+  const onStartPlay = async e => {
+    setIsPlaying(true);
+    track.play();
+  };
+  onPausePlay = async () => {
+    setIsPlaying(false);
+    track.pause();
+  };
   React.useEffect(() => {
     InCallManager.start();
     if (Platform.OS == 'android') {
@@ -45,9 +64,28 @@ export default ({navigation}) => {
             track.release();
           }}
         />
+        <View style={styles.viewPlayer}>
+          <View style={styles.playBtnWrapper}>
+            <Button
+              onPress={isPlaying ? onPausePlay : onStartPlay}
+              title={isPlaying ? 'Pause' : 'Play'}></Button>
+          </View>
+        </View>
       </View>
     );
   } else {
     return <View></View>;
   }
 };
+
+const styles = StyleSheet.create({
+  viewPlayer: {
+    marginTop: 60,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  playBtnWrapper: {
+    flexDirection: 'row',
+    marginTop: 40,
+  },
+});
